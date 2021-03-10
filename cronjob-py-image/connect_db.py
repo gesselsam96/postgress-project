a8061ce893484adca6d85f6f2eceb457c656c0b4
gesselsam96 <samge@dukes.jmu.edu>
2021-03-10 00:13:05 -0500

    py image update

--- cronjob-py-image/connect_db.py
+++ cronjob-py-image/connect_db.py
@@ -1,5 +1,17 @@
 import sqlalchemy as db
+import psycopg2
 engine = db.create_engine('postgresql://postgres:mq4Jq3C8zK@postgres-postgresql:5432/postgres')
-connection = engine.connect()
+connection = engine.connect("dbname=postgres user=postgres password=iQqsxUly9A")
 metadata = db.MetaData(bind=connection, reflect=True)
 print(metadata)
+
+# Open a database cursor
+dbCursor = dbSession.cursor();
+
+# SQL statement to create a table
+
+sqlCreateTable  = "CREATE TABLE Cities(id bigint, cityname varchar(128), latitude numeric, longitude numeric);";
+
+# Execute CREATE TABLE command
+
+dbCursor.execute(sqlCreateTable);
