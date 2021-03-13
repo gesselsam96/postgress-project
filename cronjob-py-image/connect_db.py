ad026ec3968cade542630bb00347efa91a14b999
gesselsam96 <samge@dukes.jmu.edu>
2021-03-13 04:06:47 -0500

    cronjob access to postgres

--- cronjob-py-image/connect_db.py
+++ cronjob-py-image/connect_db.py
@@ -1,17 +1,25 @@
-import sqlalchemy as db
+# ----- Example Python program to insert data into a PostgreSQL database table
+
 import psycopg2
-engine = db.create_engine('postgresql://postgres:mq4Jq3C8zK@postgres-postgresql:5432/postgres')
-connection = engine.connect("dbname=postgres user=postgres password=iQqsxUly9A")
-metadata = db.MetaData(bind=connection, reflect=True)
-print(metadata)
+
+# Open a DB session
+
+dbSession       = psycopg2.connect("dbname='postgres' user='postgres' password='jYFuBuNX6S'");
 
 # Open a database cursor
+
 dbCursor = dbSession.cursor();
+
+ 
 
 # SQL statement to create a table
 
 sqlCreateTable  = "CREATE TABLE Cities(id bigint, cityname varchar(128), latitude numeric, longitude numeric);";
 
+ 
+
 # Execute CREATE TABLE command
 
 dbCursor.execute(sqlCreateTable);
+
+
