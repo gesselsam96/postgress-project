763892fda8ad5f51e62a5b0ad1dbe2c1e9eca365
gesselsam96 <samge@dukes.jmu.edu>
2021-03-26 22:01:20 -0400

    postgres backup scripts update

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -8,3 +8,6 @@ current_time=$(date "+%Y.%m.%d")
 pg_dump --no-password -U postgres postgres > /opt/bitnami/scripts/postgresql/$current_time-$file_name
 
 echo "Backup successful"
+
+### delete files older than 5 days
+find /opt/bitnami/scripts/postgresql/ -mtime +5 -exec rm {} \;
