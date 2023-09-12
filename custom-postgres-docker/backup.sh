6159f2ff89e965cea5e868d12de9a8ab8763d5d7
gesselsam96 <samge@dukes.jmu.edu>
2023-09-11 21:49:56 -0400

    fix backup

--- custom-postgres-docker/backup.sh
+++ custom-postgres-docker/backup.sh
@@ -2,9 +2,7 @@
 
 file_name=backup.pgsql
 current_time=$(date "+%Y-%m-%d:%H")
-##Change directory to script
-#cd /opt/script
 
-pg_dump --no-password -U postgres postgres > /home/backups/$current_time-$file_name
+PGPASSWORD="password" pg_dump -U postgres postgres > /home/backups/$current_time-$file_name
 
 echo "Backup successful"
